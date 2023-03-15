468fe743799ae6fead172b0f1abacae157ae9130
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-15 07:23:48 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Bestäm om ett postnummer ligger i...
+        //   Götaland = 20-62 eller 65-66
+        //   Norrland = 80 eller större
+        //   Svealand = allt annat
+
+
+    }
+}
