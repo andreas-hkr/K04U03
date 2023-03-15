5e267dc020969fa21357a3510acdea519b28dce3
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-15 07:35:11 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Bestäm om ett postnummer ligger i...
@@ -5,6 +7,18 @@ public class Main {
         //   Norrland = 80 eller större
         //   Svealand = allt annat
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ange ett postnummer (5 siffror): ");
+        int areaCode = input.nextInt();
 
+        int twoFirst = areaCode / 1000;
+
+        if ((twoFirst >= 20 && twoFirst <= 62) || (twoFirst >= 65 && twoFirst <= 66)) {
+            System.out.println("Götaland");
+        } else if(twoFirst >= 80) {
+            System.out.println("Norrland");
+        } else {
+            System.out.println("Svealand");
+        }
     }
 }
